9752b8908fdc28055439bf6393970a4b296055ff
Avindhya Cabral <avindhya.cabral@gmail.com>
2024-03-19 05:15:48 +0530

    Basic camera page UI, responsive

--- furspace/src/pages/PageCamera.vue
+++ furspace/src/pages/PageCamera.vue
@@ -1,11 +1,58 @@
 <template>
-  <q-page class="flex flex-center">
-    <h5>Camera Page</h5>
+  <q-page class="constrain-cam q-pa-md">
+    <div class="camera-frame q-qa-md">
+      <img src="https://cdn.quasar.dev/img/parallax2.jpg" class="full-width" />
+    </div>
+    <div class="text-center q-pa-md">
+      <q-btn round color="primary" icon="fa-solid fa-camera" size="lg" />
+    </div>
+    <div class="row justify-center q-ma-md">
+      <q-input
+        v-model="post.caption"
+        label="Caption"
+        class="col col-sm-6"
+        dense
+      />
+    </div>
+    <div class="row justify-center q-ma-md">
+      <q-input
+        v-model="post.location"
+        label="Location"
+        class="col col-sm-6"
+        dense
+      >
+        <template v-slot:append>
+          <q-btn round dense flat icon="fa-solid fa-location-dot" />
+        </template>
+      </q-input>
+    </div>
+    <div class="row justify-center q-mt-lg">
+      <q-btn unelevated rounded color="primary" label="Post Image" />
+    </div>
   </q-page>
 </template>
 
 <script>
+import { uid } from "quasar";
 export default {
   name: "PageCamera",
+  data() {
+    return {
+      post: {
+        id: uid(),
+        caption: "",
+        location: "",
+        img: null,
+        date: Date.now(),
+      },
+    };
+  },
 };
 </script>
+
+<style lang="sass">
+.camera-frame
+  border: 2px solid $primary
+  border-radius: 10px
+  padding: 5px
+</style>
